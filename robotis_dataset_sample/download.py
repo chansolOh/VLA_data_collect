a5ff42155d8b18017148dfba375495a8a4a4ae30
chansolOh <chansol.oh@cubox.ai>
2026-02-02 16:06:49 +0900

    init

--- robotis_dataset_sample/download.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="ROBOTIS/omy_f3m_stack_red_cup_5",
-    repo_type="dataset",
-    local_dir="/home/cubox/ochansol/isaac_code/python/VLA_data_collect/robotis_dataset_sample/omy_f3m_stack_red_cup_5",
-    local_dir_use_symlinks=False
-)
