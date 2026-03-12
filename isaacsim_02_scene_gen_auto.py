1aedf8eda44bd30d102a430ee4e006a5e4fd2bfa
chansolOh <chansol.oh@cubox.ai>
2026-03-12 22:27:58 +0900

    123

--- isaacsim_02_scene_gen_auto.py
+++ isaacsim_02_scene_gen_auto.py
@@ -236,6 +236,7 @@ config = {}
 
 # while True:
 for episode_num in tqdm(range(args.start_num, args.end_num)):
+    episode_num = f"{episode_num:04d}"
     # episode_list = sorted([i.strip(".json") for i in os.listdir( os.path.join(dataset_path, "action") ) if i.endswith('.json')])
     # for episode_num in episode_list:
     #     if os.path.exists( os.path.join(output_path,f"rgb/{episode_num}/{full_camera.name}")):
